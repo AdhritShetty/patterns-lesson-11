73be9affc49ff8774673eff7d72927634e36784d
AdhritShetty <adhritshetty2009@gmail.com>
2025-04-16 20:14:17 +0000

    reverse triangle

--- floyd triangle.py
+++ floyd triangle.py
@@ -1,6 +1,5 @@
 n=int (input ("enter a number:"))
-for i in range (1,n+1):
-    for j in range (1,i+1):
-         print(n,end="")
-         n=n+1
-    print(n)
+for i in range (n):
+    for j in range (i+1):
+         print("*",end="")
+    print("\nreverse")
